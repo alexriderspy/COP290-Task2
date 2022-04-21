e376b033256db2f6c5d7290fd8f6e611aea2228f
alexriderspy <sreemantidey1234@gmail.com>
2022-04-22 00:32:04 +0530

    design details and socket started

---
+++ udp-sockets/constans.h
@@ -0,0 +1,32 @@
+#ifndef CONSTANS_H
+#define CONSTANS_H
+
+#include "stdbool.h"
+
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 660
+#define FRAME_TIME 16666
+#define TILE_SIZE 10
+#define GRAVITY 1
+#define MAX_VERTICAL_SPEED 100
+#define PLAYER_JUMP_POWER 25
+#define PLAYER_SPEED 2
+#define PLAYER_WIDTH 8
+#define PLAYER_HEIGHT 8
+#define BULLET_HEIGHT 8
+#define BULLET_WIDTH 8
+#define MAX_PLAYERS 10
+#define CLIENT_FRAME_TIME 15
+#define SERVER_FRAME_TIME 15
+#define X_AXIS 1
+#define Y_AXIS 0
+#define BUF_MAX 256
+#define SPAWN_X 10
+#define SPAWN_Y 610
+#define LEFT_KEY 0x01
+#define RIGHT_KEY 0x02
+#define UP_KEY 0x04
+#define DOWN_KEY 0x08
+#define ATTACK_KEY 0x10
+
+#endif
